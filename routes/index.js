661536640ed5a5983040e5f40e26bd390ea3c0b1
Unknown <nhernandezosorto@gmail.com>
2017-05-29 19:10:10 -0600

    prueba

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,44 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PROYECTO DE PRUEBA HECTOR DOBLADO' });
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto',{"img": "/images/imagen1.jpg" });
+  });
 });
 
+router.get('/api/usuarios', function(req,res,next){
+  var usuarios = [
+    {
+      "Nombre":"Hector",
+      "Apellido":"Doblado",
+      "Identidad": "0801199607333",
+      "Correo": "hectorydoblado@gmail.com",
+      "Areas":[
+          {"Area1":"Programación"},
+          {"Area2":"Base de Datos"},
+          {"Area3":"Inteligencia de Negocios"}
+        ]
+    }
+
+              ];
+  res.json(usuarios);
+
+});
+
+router.get('/prueba',function(req,res,next){
+res.render('mensaje',{"txtmensaje":"","msg":""});
+});
+
+var mensajes = [];
+
+router.post('/prueba',function(req,res,next){
+console.log(req.body);
+mensajes.push(req.body.txtmensaje);
+var prueba = mensajes.join("Prueba");
+var msgs = {"txtmensaje": req.body.txtmensaje,"msg": prueba };
+
+res.render('mensaje',msgs);
+});
 module.exports = router;
